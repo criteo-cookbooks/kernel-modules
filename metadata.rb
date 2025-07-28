a423feafe0f0c505be2cc734383919d7f40e12e9
m.colonna <m.colonna@criteo.com>
2025-07-28 18:07:32 +0200

    Bump version to 2.1.1

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ Configure and Load kernel modules.
 '
 issues_url       'https://github.com/criteo-cookbooks/kernel-modules/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/criteo-cookbooks/kernel-modules' if respond_to?(:source_url)
-version          '2.1.0'
+version          '2.1.1'
 supports         'redhat', '>= 6.0'
 supports         'centos', '>= 6.0'
 supports         'ubuntu'
